20df22e03f08cad08d11be85bf991770a0412684
Muslim217 <abumuslimsafaraliev3815@gmail.com>
2023-06-02 16:24:59 +0300

    First

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        System.out.println("Cравнение бинов HelloWorld " + (bean == bean2));
+
+
+        Cat beanCat =
+                (Cat) applicationContext.getBean("cat");
+
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Сравнение бинов CAT " + (beanCat2 == beanCat));
+
+    }
+}
